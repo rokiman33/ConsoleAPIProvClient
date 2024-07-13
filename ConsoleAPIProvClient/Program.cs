116f62961bcd174d0a31d254e978ea20956812a6
Richard Diaz <149890018+rdiazFaros@users.noreply.github.com>
2024-07-13 18:31:15 -0400

    Update Program.cs

--- ConsoleAPIProvClient/Program.cs
+++ ConsoleAPIProvClient/Program.cs
@@ -34,14 +34,14 @@ namespace RestClientApp
         //obtener el token
         static async Task<string> GetBearerToken()
         {
-            string baseUrl = "https://test.faros.cl:2083/api/v1/companies/token";
+            string baseUrl = "https://<localhost>/api/v1/companies/token";
             HttpClient client = new HttpClient();
 
             try
             {
                 //se enviar acceso para generar el token
                 var requestBody = new StringContent(
-                    "{\"rut\": \"96570750-6\", \"passwordKey\": \"d8rSIUNYwla2\"}",
+                    "{\"rut\": \"<user>\", \"passwordKey\": \"<passwd>\"}",
                     Encoding.UTF8,
                     "application/json");
 
@@ -69,7 +69,7 @@ namespace RestClientApp
         //Enviar Archivo xml
         static async Task<string> LoadDocument(string bearerToken)
         {
-            string url = "https://test.faros.cl:2083/api/v1/loadDocument/";
+            string url = "https://<localhost>/api/v1/loadDocument/";
             string xmlContent = "<DTE version='1.0'><Documento ID='2'><Encabezado><IdDoc><TipoDTE>39</TipoDTE><Folio>748</Folio><FchEmis>2014-12-13</FchEmis><IndServicio>1</IndServicio><IndMntNeto>2</IndMntNeto><PeriodoDesde>2012-12-13</PeriodoDesde><PeriodoHasta>2012-12-13</PeriodoHasta><FchVenc>2012-12-13</FchVenc></IdDoc><Emisor><RUTEmisor>str1234</RUTEmisor><RznSocEmisor>str1234</RznSocEmisor><GiroEmisor>str1234</GiroEmisor><CdgSIISucur>745</CdgSIISucur><DirOrigen>str1234</DirOrigen><CmnaOrigen>str1234</CmnaOrigen><CiudadOrigen>str1234</CiudadOrigen></Emisor><Receptor><RUTRecep>str1234</RUTRecep><CdgIntRecep>str1234</CdgIntRecep><RznSocRecep>str1234</RznSocRecep><Contacto>str1234</Contacto><DirRecep>str1234</DirRecep><CmnaRecep>str1234</CmnaRecep><CiudadRecep>str1234</CiudadRecep><DirPostal>str1234</DirPostal><CmnaPostal>str1234</CmnaPostal><CiudadPostal>str1234</CiudadPostal></Receptor><Totales><MntNeto>33</MntNeto><MntExe>33</MntExe><IVA>745</IVA><MntTotal>33</MntTotal><MontoNF>1234</MontoNF><TotalPeriodo>33</TotalPeriodo><SaldoAnterior>1234</SaldoAnterior><VlrPagar>1234</VlrPagar></Totales></Encabezado><Detalle><NroLinDet>745</NroLinDet><CdgItem><TpoCodigo>str1234</TpoCodigo><VlrCodigo>str1234</VlrCodigo></CdgItem><IndExe>1</IndExe><ItemEspectaculo>01</ItemEspectaculo><RUTMandante>str1234</RUTMandante><NmbItem>str1234</NmbItem><InfoTicket><FolioTicket>745</FolioTicket><FchGenera>2012-12-13T12:12:12</FchGenera><NmbEvento>str1234</NmbEvento><TpoTiket>str1234</TpoTiket><CdgEvento>str12</CdgEvento><FchEvento>2012-12-13T12:12:12</FchEvento><LugarEvento>str1234</LugarEvento><UbicEvento>str1234</UbicEvento><FilaUbicEvento>str</FilaUbicEvento><AsntoUbicEvento>str</AsntoUbicEvento></InfoTicket><DscItem>str1234</DscItem><QtyItem>123.45</QtyItem><UnmdItem>str1</UnmdItem><PrcItem>123.45</PrcItem><DescuentoPct>123.45</DescuentoPct><DescuentoMonto>33</DescuentoMonto><RecargoPct>123.45</RecargoPct><RecargoMonto>33</RecargoMonto><MontoItem>33</MontoItem></Detalle><SubTotInfo><NroSTI>745</NroSTI><GlosaSTI>str1234</GlosaSTI><OrdenSTI>745</OrdenSTI><SubTotNetoSTI>123.45</SubTotNetoSTI><SubTotIVASTI>123.45</SubTotIVASTI><SubTotAdicSTI>123.45</SubTotAdicSTI><SubTotExeSTI>123.45</SubTotExeSTI><ValSubtotSTI>123.45</ValSubtotSTI><LineasDeta>745</LineasDeta></SubTotInfo><DscRcgGlobal><NroLinDR>745</NroLinDR><TpoMov>D</TpoMov><GlosaDR>str1234</GlosaDR><TpoValor>%</TpoValor><ValorDR>123.45</ValorDR><IndExeDR>1</IndExeDR></DscRcgGlobal><Referencia><NroLinRef>745</NroLinRef><CodRef>str1234</CodRef><RazonRef>str1234</RazonRef><CodVndor>str1234</CodVndor><CodCaja>str1234</CodCaja></Referencia></Documento></DTE>"; // Tu XML va aquí
             var requestBody = new StringContent(
                 $"{{\"contentDocument\": \"{xmlContent}\", \"typeDocument\": \"VENTAS\", \"format\": \"XML\", \"docTypeDescription\": \"FACTURA VENTAS\"}}",
@@ -99,7 +99,7 @@ namespace RestClientApp
             try
             {
                 HttpClient client = new HttpClient();
-                var url = $"https://test.faros.cl:2083/api/v1/loadFile/upload?description={description}";
+                var url = $"https://<localhost>/api/v1/loadFile/upload?description={description}";
                 
                 using var multipartFormContent = new MultipartFormDataContent();
                 multipartFormContent.Add(new StringContent("OTROS"), "doctype");
